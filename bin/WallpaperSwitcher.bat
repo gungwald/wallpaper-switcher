79b063449d110399d215a3505ec5a740ea98ad9c
Bill Chatfield <bill_chatfield@yahoo.com>
2025-03-28 21:45:45 -0400

    Refactor into git and IDE files in top dir

---
+++ bin/WallpaperSwitcher.bat
@@ -0,0 +1,11 @@
+@echo off
+rem Builds the classpath and starts Java, specifying the main class
+rem as the same name as this file. This is completely generic.
+setlocal enabledelayedexpansion
+set MAINCLASS=%~n0
+set TOPDIR=%~dp0..
+set CLASSPATH=%TOPDIR%\classes
+title %MAINCLASS%
+for %%j in ("%TOPDIR%\lib\*.jar") do set CLASSPATH=!CLASSPATH!;%%~j
+java -classpath "%CLASSPATH%" %MAINCLASS%
+endlocal
---
+++ src/SystemParamsInfoImpl.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.WinDef.UINT_PTR;
+import com.sun.jna.win32.W32APIFunctionMapper;
+import com.sun.jna.win32.W32APITypeMapper;
+
+
+public class SystemParamsInfoImpl implements SystemParamsInfoIface {
+
+    private SystemParamsInfoIface iface = null;
+
+    public SystemParamsInfoImpl() {
+        HashMap<Object,Object> options = new HashMap<Object,Object>();
+        options.put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
+        options.put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
+        iface = (SystemParamsInfoIface) Native.loadLibrary("user32", SystemParamsInfoIface.class, options);
+    }
+
+    public boolean SystemParamsInfo(long uiAction, long uiParam, String pvParam, long fWinIni) {
+        UINT_PTR uiActionPtr = new UINT_PTR(uiAction);
+        UINT_PTR uiParamPtr = new UINT_PTR(uiParam);
+        UINT_PTR fWinIniPtr = new UINT_PTR(fWinIni);
+        return SystemParametersInfo(uiActionPtr, uiParamPtr, pvParam, fWinIniPtr);
+    }
+
+    @Override
+    public boolean SystemParametersInfo(UINT_PTR uiAction, UINT_PTR uiParam, String pvParam, UINT_PTR fWinIni) {
+        return iface.SystemParametersInfo(uiAction, uiParam, pvParam, fWinIni);
+    }
+
+}
