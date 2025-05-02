642f5e9f33711df7215c80ed1debfe3ac99035df
Bill Chatfield <bill_chatfield@yahoo.com>
2025-05-02 15:45:35 -0400

    Debugged JSON parsing

--- src/BingWallpaperAcquirer.java
+++ src/BingWallpaperAcquirer.java
@@ -1,9 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,18 +21,58 @@ public class BingWallpaperAcquirer implements PictureAcquirer {
 
     public File next() throws IOException {
         URL info = new URL(WALLPAPER_INFO_SRC_LOC);
-        logger.info("Reading Bing wallpaper metadata from " + info.toString());
-        String jsonText = (String) info.getContent();
+        logger.info("Reading Bing wallpaper metadata from: " + info.toString());
+        String jsonText = readAllChars(info);
+        logger.info("Read info: " + jsonText);
         JsonObject json = new Gson().fromJson(jsonText, JsonObject.class);
-        String wallpaperUrlText = "http://bing.com" + json.get("images[0].url").getAsString();
+        logger.info("JSON object: " + jsonText);
+        JsonObject firstImage = json.get("images").getAsJsonArray().get(0).getAsJsonObject();
+        String path = firstImage.get("url").getAsString();
+        String wallpaperUrlText = "http://bing.com" + path;
+        logger.info("images[0].url: " + jsonText);
         URL wallpaperUrl = new URL(wallpaperUrlText);
         logger.info("Reading wallpaper from " + wallpaperUrl.toString());
-        byte[] wallpaper = (byte[]) wallpaperUrl.getContent();
-        String localFileName = json.get("fullstartdate").getAsString() + "-" + json.get("title").getAsString().replace(" ", "_");
+        byte[] wallpaper = readAllBytes(wallpaperUrl);
+        String localFileName = firstImage.get("fullstartdate").getAsString() + "-" + firstImage.get("title").getAsString().replace(" ", "_");
+        mkdir(wallpaperDir);
         File localFile = new File(wallpaperDir, localFileName);
         logger.info("Writing wallpaper to " + localFile.getName());
         writeAll(localFile, wallpaper);
         return localFile;
+    }
+
+    protected void mkdir(File wallpaperDir) throws IOException {
+        if (! wallpaperDir.exists() && ! wallpaperDir.mkdir()) {
+            throw new IOException("Could not create directory. Do all the parent directories exist and do you have permission to create this directory? " + wallpaperDir.getAbsolutePath());
+        }
+    }
+
+    protected String readAllChars(URL remote) throws IOException {
+        return readAllChars(new InputStreamReader((InputStream) remote.getContent()));
+    }
+
+    protected String readAllChars(InputStreamReader reader) throws IOException {
+        char[] buffer = new char[2048];
+        StringBuilder text = new StringBuilder();
+        int count;
+        while ((count = reader.read(buffer)) != -1) {
+            text.append(buffer, 0, count);
+        }
+        return text.toString();
+    }
+
+    protected byte[] readAllBytes(URL remote) throws IOException {
+        return readAllBytes(remote.openStream());
+    }
+
+    protected byte[] readAllBytes(InputStream in) throws IOException {
+        byte[] buffer = new byte[2048];
+        ByteArrayOutputStream byteContent = new ByteArrayOutputStream();
+        int count;
+        while ((count = in.read(buffer)) != -1) {
+            byteContent.write(buffer, 0, count);
+        }
+        return byteContent.toByteArray();
     }
 
     protected void writeAll(File f, byte[] wallpaper) throws IOException {
