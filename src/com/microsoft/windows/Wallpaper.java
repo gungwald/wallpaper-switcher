eafba1a3cef915be74d4a75b5f8d3a736fafd39b
Bill Chatfield <bill_chatfield@yahoo.com>
2025-05-04 03:22:30 -0400

    Error check after wallpaper set

--- src/com/microsoft/windows/Wallpaper.java
+++ src/com/microsoft/windows/Wallpaper.java
@@ -2,6 +2,8 @@ package com.microsoft.windows;
 
 import com.sun.jna.Native;
 import com.sun.jna.NativeLibrary;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.Win32Exception;
 import com.sun.jna.platform.win32.WinDef.UINT;
 import com.sun.jna.win32.W32APIOptions;
 
@@ -14,13 +16,19 @@ import java.util.Map;
  */
 public class Wallpaper {
 
-    /** <code>uiAction</code> value for the SystemParametersInfo function that specifies that the wallpaper should be set */
+    /**
+     * <code>uiAction</code> value for the SystemParametersInfo function that specifies that the wallpaper should be set
+     */
     @SuppressWarnings("SpellCheckingInspection")
     protected static final long SPI_SETDESKWALLPAPER = 0x0014;
-    /** Writes the new system-wide parameter setting to the user profile */
+    /**
+     * Writes the new system-wide parameter setting to the user profile
+     */
     @SuppressWarnings("SpellCheckingInspection")
     protected static final long SPIF_UPDATEINIFILE = 0x01;
-    /** Broadcasts the WM_SETTINGCHANGE message after updating the user profile */
+    /**
+     * Broadcasts the WM_SETTINGCHANGE message after updating the user profile
+     */
     @SuppressWarnings("SpellCheckingInspection")
     protected static final long SPIF_SENDWININICHANGE = 0x02;
 
@@ -29,7 +37,7 @@ public class Wallpaper {
      * See the code for W32APIOptions.DEFAULT_OPTIONS.
      */
     @SuppressWarnings("unchecked") // There is nothing more that can be done to fix this warning.
-    protected static final Map<String,Object> WALLPAPER_OPTIONS = new HashMap<String, Object>(W32APIOptions.DEFAULT_OPTIONS) {
+    protected static final Map<String, Object> WALLPAPER_OPTIONS = new HashMap<String, Object>(W32APIOptions.DEFAULT_OPTIONS) {
         {
             // An example of how to add more options because this is really hard to figure out. I suggest leaving
             // this here for that reason.
@@ -39,21 +47,22 @@ public class Wallpaper {
 
     // Loads the user32.dll library so that the SystemParametersInfo function can be accessed.
     static {
-        Native.register( NativeLibrary.getInstance( "user32", WALLPAPER_OPTIONS) );
+        Native.register(NativeLibrary.getInstance("user32", WALLPAPER_OPTIONS));
     }
 
     /**
      * This is the Win32
      * <a href="https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfow">SystemParametersInfo</a>
-     * function. It is used to set the wallpaper, if you perform the necessary softly spoken magic spells.
+     * function. It can be used to set the wallpaper, if you perform the appropriate softly spoken magic spells.
      * It is declared in WinUser.h and defined in user32.dll.
+     *
      * @param uiAction Specifies what operation to perform. In this case, the value SPI_SETDESKWALLPAPER is passed,
      *                 which tells Windows to set the wallpaper.
-     * @param uiParam Apparently, useless for setting the wallpaper, as the value is 0
-     * @param pvParam The name of the file containing an image, if you're setting the wallpaper
-     * @param fWinIni A bitmask of SPIF_* values, specifying whether the user profile is to be updated, and if so,
-     *                whether the WM_SETTINGCHANGE message is to be broadcast to all top-level windows to notify them
-     *                of the change.
+     * @param uiParam  Apparently, useless for setting the wallpaper, as the value is 0
+     * @param pvParam  The name of the file containing an image, if you're setting the wallpaper
+     * @param fWinIni  A bitmask of SPIF_* values, specifying whether the user profile is to be updated, and if so,
+     *                 whether the WM_SETTINGCHANGE message is to be broadcast to all top-level windows to notify them
+     *                 of the change.
      * @return When setting the wallpaper, this will be false if it fails, true if it succeeds. To get extended error
      * information, call
      * <a href="https://learn.microsoft.com/en-us/windows/desktop/api/errhandlingapi/nf-errhandlingapi-getlasterror">GetLastError</a>.
@@ -61,18 +70,25 @@ public class Wallpaper {
     @SuppressWarnings("SpellCheckingInspection")
     protected static native boolean SystemParametersInfo(UINT uiAction, UINT uiParam, String pvParam, UINT fWinIni);
 
-    public Wallpaper() {}
+    public Wallpaper() {
+    }
 
     /**
      * Sets the damn wallpaper.
+     *
      * @param wallpaper File containing the image for the wallpaper
      */
     public void set(File wallpaper) {
-        SystemParametersInfo(
-                new UINT(SPI_SETDESKWALLPAPER),
-                new UINT(0),
-                wallpaper.getAbsolutePath(),
-                new UINT(SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
-        );
+        boolean success =
+                SystemParametersInfo(
+                        new UINT(SPI_SETDESKWALLPAPER),
+                        new UINT(0),
+                        wallpaper.getAbsolutePath(),
+                        new UINT(SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
+                );
+
+        if (! success) {
+            throw new Win32Exception(Kernel32.INSTANCE.GetLastError());
+        }
     }
 }
