2ceba47420bc6590e1fa2958dbcca52e0e438111
Bill Chatfield <bill_chatfield@yahoo.com>
2025-05-04 02:05:22 -0400

    Get file name from id in wallpaper URL

--- src/BingWallpaperAcquirer.java
+++ src/BingWallpaperAcquirer.java
@@ -3,6 +3,11 @@ import com.google.gson.JsonObject;
 
 import java.io.*;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -31,14 +36,29 @@ public class BingWallpaperAcquirer implements PictureAcquirer {
         String wallpaperUrlText = "http://bing.com" + path;
         logger.info("images[0].url: " + jsonText);
         URL wallpaperUrl = new URL(wallpaperUrlText);
-        logger.info("Reading wallpaper from " + wallpaperUrl.toString());
         byte[] wallpaper = readAllBytes(wallpaperUrl);
-        String localFileName = firstImage.get("fullstartdate").getAsString() + "-" + firstImage.get("title").getAsString().replace(" ", "_");
+        logger.info("Reading wallpaper from " + wallpaperUrl.toString());
+        String localFileName = splitQuery(wallpaperUrl).get("id").get(0);
         mkdir(wallpaperDir);
         File localFile = new File(wallpaperDir, localFileName);
         logger.info("Writing wallpaper to " + localFile.getName());
         writeAll(localFile, wallpaper);
         return localFile;
+    }
+
+    public Map<String, List<String>> splitQuery(URL url) throws UnsupportedEncodingException {
+        final Map<String, List<String>> query_pairs = new LinkedHashMap<String, List<String>>();
+        final String[] pairs = url.getQuery().split("&");
+        for (String pair : pairs) {
+            final int idx = pair.indexOf("=");
+            final String key = idx > 0 ? URLDecoder.decode(pair.substring(0, idx), "UTF-8") : pair;
+            if (!query_pairs.containsKey(key)) {
+                query_pairs.put(key, new LinkedList<String>());
+            }
+            final String value = idx > 0 && pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), "UTF-8") : null;
+            query_pairs.get(key).add(value);
+        }
+        return query_pairs;
     }
 
     protected void mkdir(File wallpaperDir) throws IOException {
